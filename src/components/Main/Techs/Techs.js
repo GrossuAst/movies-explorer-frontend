d960207da718c71fe73e7a6c0d7a9925bcd47607
Maxim Grossu <maxgrossu@yandex.ru>
2023-07-30 19:10:20 +0300

    сверстан компонент Techs

---
+++ src/components/Main/Techs/Techs.js
@@ -0,0 +1,40 @@
+import './Techs.css';
+
+function Techs() {
+  return (
+    <section className="technologies">
+        <div className='technologies__wrapper'>
+            <h2 className='technologies__title'>Технологии</h2>
+            <div>
+                <h3 className='technologies__subtitle'>7 технологий</h3>
+                <p className='technologies__description'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+            </div>
+            <ul className='technologies__list'>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>HTML</p>
+                </li>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>CSS</p>
+                </li>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>JS</p>
+                </li>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>React</p>
+                </li>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>Git</p>
+                </li>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>Express.js</p>
+                </li>
+                <li className='technologies__bullet'>
+                    <p className='technologies__tech'>mongoDB</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+  );
+}
+
+export default Techs;
